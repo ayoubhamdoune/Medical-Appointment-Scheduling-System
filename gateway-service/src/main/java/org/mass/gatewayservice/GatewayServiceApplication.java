de480eb8113e3695ca6ee86cf98cadcb267e1862
hamdouneayoub <ayoubhamdoune.pro@gmail.com>
2024-10-28 20:19:22 +0100

    Gateway and eureka server configuration

--- gateway-service/src/main/java/org/mass/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/org/mass/gatewayservice/GatewayServiceApplication.java
@@ -5,11 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
 import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
 import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
-@Configuration
 public class GatewayServiceApplication {
 
     public static void main(String[] args) {
@@ -20,7 +21,12 @@ public class GatewayServiceApplication {
     DiscoveryClientRouteDefinitionLocator dynamicRoutes(ReactiveDiscoveryClient rdc,
                                                         DiscoveryLocatorProperties dlp){
         return new DiscoveryClientRouteDefinitionLocator(rdc,dlp);
-    }
+    }/*
+    @Bean
+    RouteLocator routeLocator(RouteLocatorBuilder builder){
+        return builder.routes()
+                .route((r)->r.path("/patients/**").uri("http://localhost:8083/")).build();
+    }*/
 
 }
 
