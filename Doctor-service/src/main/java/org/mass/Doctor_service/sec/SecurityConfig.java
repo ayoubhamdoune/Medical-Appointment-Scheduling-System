0b25887589e17d5cd98299d30b256448fb2cb44f
MP40 <oussamasabir36@gmail.com>
2024-11-22 00:31:44 +0100

    Dockerize Micro Services with permetAll in Keycloak

--- Doctor-service/src/main/java/org/mass/Doctor_service/sec/SecurityConfig.java
+++ Doctor-service/src/main/java/org/mass/Doctor_service/sec/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig {
                 .sessionManagement(sm->sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .csrf(csrf->csrf.disable())
                 //.authorizeHttpRequests(ar->ar.requestMatchers("/api/doctor/**").hasAuthority("USER"))
-                .authorizeHttpRequests(ar->ar.anyRequest().authenticated())
+                .authorizeHttpRequests(ar->ar.anyRequest().permitAll())
                 .oauth2ResourceServer(ors->ors.jwt(jwt->jwt.jwtAuthenticationConverter(jwtAuthConverter)))
                 .build();
     }
